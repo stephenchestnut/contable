611dd6cb2dc60571bd069f861597d3de2460b3e5
Steve Chestnut <schestnut@gmail.com>
2014-07-15 23:09:17 +0000

    added marginsWithCellBoundsClass

--- src/main/scala/contable/margins.scala
+++ src/main/scala/contable/margins.scala
@@ -3,17 +3,29 @@ package contable
 
 import breeze.linalg._
 
-class Margins(val row_sums: Seq[Int], val col_sums: Seq[Int]) {
+class Margins(row_sums: Seq[Int], col_sums: Seq[Int]) {
 
   val r = new DenseVector(row_sums.toArray)
   val c = new DenseVector(col_sums.toArray)
   val m = r.length
   val n = c.length
 
-  def isFeasible = true
+  def isFeasible = ( (m>0) && (n>0) && (sum(r) == sum(c)) && all(r :>= 0) && all(c :>= 0) )
 
   def check(X: DenseMatrix[Int]) = 
     ((X.rows == m) && (X.cols == n)
      && (sum(X,Axis._0) == r.toDenseMatrix)
      && (sum(X,Axis._1) == c))
+}
+
+class MarginsWithCellBounds(row_sums: Seq[Int], 
+                            col_sums: Seq[Int],
+                            cell_bounds: Matrix[Int]) extends Margins(row_sums,col_sums) {
+
+  val B = cell_bounds.toDenseMatrix
+
+  override def isFeasible = super.isFeasible //check the flow problem instead
+
+  override def check(X: DenseMatrix[Int]) = (super.isFeasible && all(X :<= B))
+
 }
